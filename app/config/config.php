d30bda5d38ea86603f08b79ea5b21a597f4e3cd5
aihtshamali <14024020154@umt.edu.pk>
2019-01-29 22:36:09 +0500

    FirstComit

--- app/config/config.php
+++ app/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost:8080/pos';
+$config['base_url'] = 'http://localhost/pos';
 
 /*
 |--------------------------------------------------------------------------
